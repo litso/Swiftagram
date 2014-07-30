61404b198045131f7f8e8999651bbdb6e2a363d1
Robert Manson <robmanson@gmail.com>
2014-07-29 22:16:09 -0700

    Initial Commit

---
+++ Swiftagram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swiftagram
+//
+//  Created by Robert Manson on 7/29/14.
+//  Copyright (c) 2014 Robert Manson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
